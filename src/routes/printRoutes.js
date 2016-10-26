e4841eed9b342889252942359dede1a2323b0fde
Tejasgirase <tejas.girase@tops-int.com>
2016-10-26 11:03:49 +0530

    change in print medication path added

--- src/routes/printRoutes.js
+++ src/routes/printRoutes.js
@@ -1,6 +1,7 @@
 var express = require("express"),
 		printRouter = express.Router();
 var printController = require("../controller/printController")();
-
+console.log("Routes enter");
 printRouter.route("/appointment").get(printController.printAppointment);
+printRouter.route("/medications").get(printController.printMedication);
 module.exports = printRouter;
