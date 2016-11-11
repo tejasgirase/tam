0b0714a431645250048264d0a9a34e90035410c5
Tejasgirase <tejas.girase@tops-int.com>
2016-11-11 20:11:03 +0530

    typo fixed in print controller

--- src/controller/printController.js
+++ src/controller/printController.js
@@ -18,7 +18,7 @@ var css_path    = path.join(__dirname,'css/'),
 		images_path = path.join(__dirname,'images/'),
 		view_path   = path.join(__dirname,'../views/'),
 		Cloudant    = require(nconf.APP_MODULE),
-		cloudant    = Cloudant(nconf.DB_PROTOCOL+"://"+CLOUDANT_API_KEY+":"+CLOUDANT_PASSWORD+"@"+nconf.DB_URL),
+		cloudant    = Cloudant(nconf.DB_PROTOCOL+"://"+CLOUDANT_API_KEY+":"+CLOUDANT_PASSWORD+"@"+nconf.DB_URL);
 
 // var EH_IP       = nconf.External_Handler_IP;
 // var EH_Username = nconf.External_Handler_Username;
