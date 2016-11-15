62031dc2e792eb3483b33991045f47d2129ac915
Tejasgirase <tejas.girase@tops-int.com>
2016-11-15 19:27:20 +0530

    merge auto completer hospital name and dhp_code

--- tamsa_replicated/views/getDhpHospital/map.js
+++ tamsa_replicated/views/getDhpHospital/map.js
@@ -1,6 +1,8 @@
 function(doc) {
    if(doc.type =='user' && doc.first_name) {
     emit([doc.dhp_code,doc.hospital_affiliated,doc.hospital_type,doc.city,doc.state,doc.country]);
-    emit([doc.hospital_type,doc.hospital_affiliated,doc.dhp_code,doc.city,doc.state,doc.country]);
+    // emit([doc.hospital_type,doc.hospital_affiliated,doc.dhp_code,doc.city,doc.state,doc.country]);
+    emit([doc.hospital_type,doc.dhp_code,doc.hospital_affiliated,doc.city,doc.state,doc.country,doc.hospital_phone,doc.hospital_email,doc.hospital_affiliated,doc.dhp_code]);
+    emit([doc.hospital_type,doc.hospital_affiliated,doc.dhp_code,doc.city,doc.state,doc.country,doc.hospital_phone,doc.hospital_email,doc.hospital_affiliated,doc.dhp_code]);
    }
   }
