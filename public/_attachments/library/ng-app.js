1657b0e715c00cd714de1f2b49e31ad27b0b2781
Tejasgirase <tejas.girase@tops-int.com>
2016-12-02 19:36:02 +0530

    working on telepediatric

---
+++ public/_attachments/library/ng-app.js
@@ -0,0 +1,28 @@
+var app = angular.module('tamsa_pediatric', ['ui.router','angularUtils.directives.dirPagination']);
+
+app.config(function($stateProvider, $urlRouterProvider, paginationTemplateProvider,$locationProvider){
+  paginationTemplateProvider.setPath('template/dirPagination.tpl.html');
+  console.log("tejas");
+  // $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('/', {
+    url: '/',
+    views: {
+      'index-view': {
+        templateUrl: 'partials/header.html',
+      }
+    }
+  }).state('/signup', {
+    url: '/signup',
+    views: {
+      'index-view': {
+        templateUrl: 'template/sign-up.html',
+        controller: 'signUpController'
+      },
+      'footer':{
+        templateUrl: 'template/footer.html',
+      }
+    }
+  });
+});
---
+++ public/app/lib/ng-app.js
@@ -0,0 +1,31 @@
+var app = angular.module('tamsa_pediatric', ['ui.router','angularUtils.directives.dirPagination']);
+
+app.config(function($stateProvider, $urlRouterProvider, paginationTemplateProvider,$locationProvider){
+  paginationTemplateProvider.setPath('template/dirPagination.tpl.html');
+  // $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('/', {
+    url: '/',
+    views: {
+      'index-view': {
+        templateUrl: 'template/login.html',
+        // controller: 'indexAppController'
+      },
+      'footer':{
+        templateUrl: 'template/footer.html',
+      }
+    }
+  }).state('/signup', {
+    url: '/signup',
+    views: {
+      'index-view': {
+        templateUrl: 'template/sign-up.html',
+        controller: 'signUpController'
+      },
+      'footer':{
+        templateUrl: 'template/footer.html',
+      }
+    }
+  });
+});
